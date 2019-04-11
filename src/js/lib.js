c9bf4cc1425bead294e64fe2c8f5914cb08e9ab7
Dias <saiddias.14@gmail.com>
2019-04-11 20:43:26 +0300

    fixed #3

--- src/js/lib.js
+++ src/js/lib.js
@@ -1,10 +1,10 @@
 export function bonus(items) {
-    let SumBonus = 0;
+    let sumBonus = 0;
     for (const item of items) {
        const limit = 10000;
        if (item > limit) {
-       SumBonus += (item - limit) * 0.05;
+       sumBonus += (item - limit) * 0.05;
        }
     }
-    return SumBonus;
+    return sumBonus;
 }
